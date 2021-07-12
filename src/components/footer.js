cf9f456edf59dc96c5a50e80b2619d52cb70c356
greenyarnball <77083012+greenyarnball@users.noreply.github.com>
2021-07-12 12:40:59 +0900

    header

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,19 +6,24 @@ const Footer = () => {
     <footer>
       <section>
         <div className="address">
-          <h3>WHERE ARE WE?</h3>
-          <span>65 Park Row</span>
-          <span>Edinburgh, EH1 4PE</span>
+          <h3>店舗情報</h3>
+          <span>三重県四日市市</span>
+          <span>富田栄町２９−１</span>
         </div>
         <div className="hours">
-          <h3>HOURS</h3>
-          <span>Weekdays / 8-5</span>
-          <span>Weekdays / 10-5</span>
+          <h3>営業時間</h3>
+          <span>13:00~21:00</span>
+          <span>休日：　水木</span>
         </div>
         <div className="contact">
           <h3>CONTACT</h3>
-          <span>01632 960966</span>
-          <span>@yourSocialName</span>
+          <a href="https://asagiroast.theshop.jp/">
+          <span>https://asagiroast.theshop.jp/</span>
+          </a>
+          <a href="https://www.instagram.com/asagi_handroast/?hl=ja">
+            <span>@asagi_handroast</span>
+          </a>
+          
         </div>
       </section>
       <p>Built by
